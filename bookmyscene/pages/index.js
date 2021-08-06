afff6950250d58ab48d51df6ccea7c45a67b3cf6
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-08-07 01:51:31 +0530

    putting it all together (cards and footer)

--- bookmyscene/pages/index.js
+++ bookmyscene/pages/index.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
-import Banner from "../components/Banner";
-import Header from "../components/Header";
+//components
+import Header from "../Components/Header";
+import Banner from "../Components/Banner";
+import SmallCard from "../Components/SmallCard";
+import MediumCard from "../Components/MediumCard";
+import LargeCard from "../Components/LargeCard";
+import Footer from "../Components/Footer";
 
-export default function Home() {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -12,6 +17,43 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map((item) => (
+              <MediumCard key={item.img} img={item.img} title={item.title} />
+            ))}
+          </div>
+        </section>
+        <div className="">
+          <LargeCard
+            img="https://links.papareact.com/4cj"
+            title="The Greatest Outdoors"
+            description="Whislist curated by Airbnb"
+            buttonText=" Get Inspired"
+          />
+        </div>
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
