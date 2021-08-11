7afe6e04a44b83d17f05fc4bb0e7c237746b06ba
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-08-11 15:15:27 +0530

    Style : header

--- bookmyscene/components/Header.js
+++ bookmyscene/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import {
   SearchIcon,
@@ -8,8 +8,27 @@ import {
 } from "@heroicons/react/solid";
 
 function Header() {
+  const [handleShow, setHandleShow] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      if (window.scrollY > 80) {
+        setHandleShow(true);
+      } else setHandleShow(false);
+    };
+    window.addEventListener("scroll", listener);
+
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  }, []);
+
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md: px-10 ">
+    <header
+      className={`fixed top-0 z-40 grid w-screen grid-cols-1  transition duration-100 ease-out p-5 ${
+        handleShow ? "bg-white shadow-md" : ""
+      } md:grid-cols-3  z-50 grid grid-flow-row grid-cols-2 p-5 md:px-10 sm:grid-cols-3 text-white`}
+    >
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src="https://links.papareact.com/qd3"
@@ -27,7 +46,7 @@ function Header() {
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
       <div className="flex items-center justify-end space-x-4 text-gray-500 ">
-        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <p className="hidden md:inline cursor-pointer ">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
         <div className="flex border-2 items-center space-x-2 rounded-full p-2">
           <MenuIcon className="h-6 cursor-pointer" />
