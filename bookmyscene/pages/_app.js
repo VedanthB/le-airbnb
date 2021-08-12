dca8af584f2c2d40ba3c689759e6c97977d35028
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-08-13 04:22:14 +0530

    mapbox css

--- bookmyscene/pages/_app.js
+++ bookmyscene/pages/_app.js
@@ -2,6 +2,7 @@ import "tailwindcss/tailwind.css";
 import "./_globals.css";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
+import "./mapbox-gl.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
